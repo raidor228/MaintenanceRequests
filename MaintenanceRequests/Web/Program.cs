a1881c3ad228968b399327e316c614abd540d3a2
iamraidor <aleturovichyevgeny@gmail.com>
2026-09-16 23:40:17 +0300

    Use repair request repository and service

--- MaintenanceRequests/Web/Program.cs
+++ MaintenanceRequests/Web/Program.cs
@@ -1,4 +1,7 @@
+using MaintenanceRequests.Application.Interfaces;
+using MaintenanceRequests.Application.Services;
 using MaintenanceRequests.Infrastructure.Data;
+using MaintenanceRequests.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder();
@@ -7,6 +10,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IRepairRequestRepository, RepairRequestRepository>();
+builder.Services.AddScoped<IRepairRequestService, RepairRequestService>();
 
 var app = builder.Build();
 
