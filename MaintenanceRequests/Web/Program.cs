7894bf1aa19458b8e5da28cf2f08317b3c762bcd
iamraidor <aleturovichyevgeny@gmail.com>
2026-09-16 23:27:16 +0300

    Use DbContext and ConnectionString

--- MaintenanceRequests/Web/Program.cs
+++ MaintenanceRequests/Web/Program.cs
@@ -1,4 +1,12 @@
+using MaintenanceRequests.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
