a0d4efb67305536ac77694b7537e9edd802a5623
iamraidor <aleturovichyevgeny@gmail.com>
2026-09-17 00:45:49 +0300

    Change wwwroot folder location, connect controllers

--- MaintenanceRequests/Web/Program.cs
+++ MaintenanceRequests/Web/Program.cs
@@ -4,8 +4,12 @@ using MaintenanceRequests.Infrastructure.Data;
 using MaintenanceRequests.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder();
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+{
+    WebRootPath = "Web/wwwroot"
+});
 
+builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -13,7 +17,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<IRepairRequestRepository, RepairRequestRepository>();
 builder.Services.AddScoped<IRepairRequestService, RepairRequestService>();
+builder.Services.AddScoped<IRepairCategoryRepository, RepairCategoryRepository>();
 
 var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
