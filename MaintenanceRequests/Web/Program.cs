b731c65ebdeef5cdcc87a2ceec8bb9d02bd82ef9
iamraidor <aleturovichyevgeny@gmail.com>
2026-09-17 01:25:45 +0300

    Use Identity

--- MaintenanceRequests/Web/Program.cs
+++ MaintenanceRequests/Web/Program.cs
@@ -1,8 +1,10 @@
 using MaintenanceRequests.Application.Interfaces;
 using MaintenanceRequests.Application.Services;
+using MaintenanceRequests.Domain.Entities;
 using MaintenanceRequests.Infrastructure.Data;
 using MaintenanceRequests.Infrastructure.Repositories;
 using MaintenanceRequests.Web.Middleware;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
@@ -15,6 +17,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services
+    .AddIdentityCore<ApplicationUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager();
 
 builder.Services.AddScoped<IRepairRequestRepository, RepairRequestRepository>();
 builder.Services.AddScoped<IRepairRequestService, RepairRequestService>();
