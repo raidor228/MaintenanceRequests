614333c79bd14d2e1e0f49585d0b586486ebbf1e
iamraidor <aleturovichyevgeny@gmail.com>
2026-09-17 01:47:02 +0300

    Add login and register dtos

---
+++ MaintenanceRequests/Application/DTOs/Auth/LoginDto.cs
@@ -0,0 +1,7 @@
+namespace MaintenanceRequests.Application.DTOs.Auth;
+
+public class LoginDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
---
+++ MaintenanceRequests/Application/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,8 @@
+namespace MaintenanceRequests.Application.DTOs.Auth;
+
+public class RegisterDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
