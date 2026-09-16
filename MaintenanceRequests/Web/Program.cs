8b7f5043f0b9c8206073d694707d1e1daf41157f
iamraidor <aleturovichyevgeny@gmail.com>
2026-09-17 01:49:24 +0300

    Use roles

--- MaintenanceRequests/Web/Program.cs
+++ MaintenanceRequests/Web/Program.cs
@@ -2,6 +2,7 @@ using MaintenanceRequests.Application.Interfaces;
 using MaintenanceRequests.Application.Services;
 using MaintenanceRequests.Domain.Entities;
 using MaintenanceRequests.Infrastructure.Data;
+using MaintenanceRequests.Infrastructure.Identity;
 using MaintenanceRequests.Infrastructure.Repositories;
 using MaintenanceRequests.Web.Middleware;
 using Microsoft.AspNetCore.Identity;
@@ -31,6 +32,11 @@ builder.Services.AddScoped<IRepairRequestService, RepairRequestService>();
 builder.Services.AddScoped<IRepairCategoryRepository, RepairCategoryRepository>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    await IdentitySeeder.SeedAsync(scope.ServiceProvider);
+}
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
