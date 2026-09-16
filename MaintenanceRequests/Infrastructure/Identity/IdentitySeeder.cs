3983cd5c7ea1559689bc2cd5831ca3c675da5480
iamraidor <aleturovichyevgeny@gmail.com>
2026-09-17 01:47:48 +0300

    Create IdentitySeeder.cs

---
+++ MaintenanceRequests/Infrastructure/Identity/IdentitySeeder.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace MaintenanceRequests.Infrastructure.Identity;
+
+public static class IdentitySeeder
+{
+    public static async Task SeedAsync(IServiceProvider services)
+    {
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        string[] roles =
+        {
+            "Client",
+            "Worker",
+            "Admin"
+        };
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+}
